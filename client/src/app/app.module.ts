ac5a01636b0cb18201c38af75488e80d4cb3519c
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-09 08:50:55 -0800

    coded the register form using Angular Reactive forms and bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { MembersComponent } from './members/members.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    MembersComponent
+    MembersComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+	registerForm: FormGroup;
+	post: any;
+	description: string = '';
+	name: string = '';
+
+	constructor( private fb: FormBuilder ) { 
+		this.registerForm = fb.group({
+			'name': [ null, Validators.compose([ Validators.required, Validators.minLength(2), Validators.maxLength(12)])],
+			'description': [ null, Validators.compose([ Validators.required, Validators.minLength(4)])]
+		})
+	}
+
+	ngOnInit() {
+	}
+
+	register( userInfo ) {
+		this.name = userInfo.name;
+		this.description = userInfo.description;
+
+		console.log(userInfo)
+		this.registerForm.reset()
+	}
+
+}
